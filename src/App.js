dff2a0670f343206267ead7ee209a9dc382e0189
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-11 22:21:55 +0100

    Set up localstorage to save theme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { useState, useEffect } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
-  const [theme, setTheme] = useState("light-theme");
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
 
   useEffect(() => {
     document.documentElement.className = theme;
-    console.log(document.documentElement);
+    localStorage.setItem("theme", theme);
   }, [theme]);
   return (
     <main>
       <nav className="nav-center">
         <h1>Overreacted</h1>
-        <button className="btn">Toggle</button>
+        <button className="btn" onClick={toggleTheme}>
+          Toggle
+        </button>
       </nav>
       <section className="articles">
         {data.map((item) => {
